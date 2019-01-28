4d82fbe978397d097b9dc549b78a52cc3c44983e
Michael Soares <mikey.soares@gmail.com>
2019-01-28 22:59:55 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,10 @@
 class Cat
   # code goes here
-  attr_accessor :owner, :cat, :mood
+  attr_accessor :owner, :mood
   attr_reader :name
-  def initialize(name)
+  def initialize(name, mood = "nervous")
     @name = name
-    @mood = "nervous"
+    @mood = mood
 
   end
 end
