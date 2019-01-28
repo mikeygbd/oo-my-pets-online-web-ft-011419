f9f4cc339b12a549da0522abaafdc863c8bf5399
Michael Soares <mikey.soares@gmail.com>
2019-01-28 22:17:11 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,10 @@
 class Cat
   # code goes here
+  attr_accessor :owner, :cat, :mood
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+
+  end
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,10 @@
 class Dog
   # code goes here
+  attr_accessor :owner, :dog, :mood
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @dog = dog
+    @mood = "nervous"
+  end
 end
