0c9dcca0bca5def44e0db2454c48cda17e52281d
Michael Soares <mikey.soares@gmail.com>
2019-01-28 22:59:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
   # code goes here
-  attr_accessor  :mood
+  attr_accessor :owner, :mood
   attr_reader :name
   def initialize(name, mood = "nervous")
     @name = name
-    @mood = @mood
+    @mood = mood
   end
 
 
