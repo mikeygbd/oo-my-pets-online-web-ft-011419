490310b4724ec97a29fe00036d8a211018cd62b1
Michael Soares <mikey.soares@gmail.com>
2019-01-28 23:11:28 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -72,7 +72,7 @@ class Owner
   end
 
   def list_pets
-    "I have #{@pets[:fish].size} fish, #{@pets[:dogs].size} dog(s), and #{@pets[:cats].size} cat(s)."
+    "I have #{@pets[:fish].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
 
   end
 
